2c84c39998dcb23ce4c16bdaee9400c65d4145b6
nikhil4228 <nikhil4228@gmail.com>
2016-11-05 01:46:00 -0500

    complete project
    after all the changes

--- NiceIceCream/NiceIceCream/App_Start/RouteConfig.cs
+++ NiceIceCream/NiceIceCream/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NiceIceCream
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
